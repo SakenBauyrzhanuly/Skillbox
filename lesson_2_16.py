0efda359ad7ae012bbadb37489ad72e2adba2f09
Saken.Sailaubek <sakena.kz@gmail.com>
2023-03-06 14:54:20 +0600

    менин комитым

--- lesson_2_16.py
+++ lesson_2_16.py
@@ -2,10 +2,10 @@ def sum_factorials(num):
     part_factorial = 1
     summa = 0
 
-    for i in range(1,num + 1):
+    for i in range(1, num + 1):
         part_factorial *= i
         summa += part_factorial
-    return  summa
+    return summa
 
 
 n = int(input('Введите число: '))
