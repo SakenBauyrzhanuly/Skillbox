a45d4a8d4959d418cce94fb73882a38128a97089
Saken.Sailaubek <sakena.kz@gmail.com>
2023-03-10 16:38:20 +0600

    Решил 1.9 Практическая работа Задача номер 2 2. Сумма и разность 2.3 Списки: работа со строками"

--- Module 2/lesson_23tasks.py
+++ Module 2/lesson_23tasks.py
@@ -1,19 +1,27 @@
-print('Задача 1. Текстовый редактор: возвращение')
-word = input('Введите слово: ')
-# res = word.replace(':', ';')
+# print('Задача 1. Текстовый редактор: возвращение')
+# word = input('Введите слово: ')
+# # res = word.replace(':', ';')
+# # print(res)
+#
+# replace_sym = input('Чем заменяем: ')
+# text = ':'
+# count = 0
+# res = ''
+# for i in range(len(word)):
+#     if word[i] == text:
+#         res = word.replace(word[i], ';')
+#         count += 1
+#
+#
+# print('Кол-во замен: ', count)
 # print(res)
 
-replace_sym = input('Чем заменяем: ')
-text = ':'
-count = 0
-res = ''
-for i in range(len(word)):
-    if word[i] == text:
-        res = word.replace(word[i], ';')
-        count += 1
-
-
-print('Кол-во замен: ', count)
-print(res)
-
 print('Задача 2. Соседи')
+word = input('Введите строку: ')
+r_n = int(input('Номер символа: '))
+
+print(word[r_n-1-1], '  ', word[r_n-1+1])
+
+# for i in range(len(word)):
+
+
